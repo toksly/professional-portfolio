be9f3975d26c8816aae99fd54c9333dc91873548
toksly <nwizutoks@gmail.com>
2021-03-29 12:21:40 -0500

    startedwork on my portfolio

---
+++ src/components/About/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function About() {
+	return (
+		<section className="my-5">
+			<div className="my-2">
+				<div className="profile-img my-5">
+					<i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
+				</div>
+				<p>
+					My name is Tochukwu Sylvester Nwizu. I attended the Vanderbilt
+					University coding Boot Camp. As an up coming full stack web developer,
+					I am willing to work in an environment that will allow me to
+					contribute as well as help me to grow and become a successful full
+					stack web developer. I have a very strong work ethic, and I know the
+					importance of continuously striving for improvement. For this reason,
+					I am open and willing to trying new things outside of my comfort zone.
+					I intend using my new learned skill set in solving real world problems
+					through collaboration with my coleques. I believe with team work, no
+					problem will be too difficult to tackle. Moreover, My experience
+					working in healthcare setting has helped me to cultivate the habit of
+					working calmly even in a highly stressful environment.
+				</p>
+			</div>
+		</section>
+	);
+}
+
+export default About;
---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import Project from "../Project";
+
+function Portfolio() {
+	// Replace links with deployed projects and GitHub repos
+	const [projects] = useState([
+		{
+			name: "surf-report",
+			description:
+				"Javascript app that generates random password",
+			link: "https://toksly.github.io/Password-Generator/",
+			repo: "https://github.com/toksly/Password-Generator/",
+		},
+		{
+			name: "pastel-puzzels",
+			description: "MERN Stack",
+			link: "https://github.com",
+			repo: "https://github.com",
+		},
+		{
+			name: "run-buddy",
+			description: "HTML/CSS",
+			link: "https://github.com",
+			repo: "https://github.com",
+		},
+		{
+			name: "led-wall",
+			description: "Node/IoT",
+			link: "https://github.com",
+			repo: "https://github.com",
+		},
+		{
+			name: "calculator",
+			description: "React/JavaScript/CSS",
+			link: "https://github.com",
+			repo: "https://github.com",
+		},
+	]);
+
+	return (
+		<div>
+			<div className="flex-row">
+				{projects.map((project, idx) => (
+					<Project project={project} key={"project" + idx} />
+				))}
+			</div>
+		</div>
+	);
+}
+
+export default Portfolio;
