3a30e65705f1aaa7f63094e8378b7974a49281e3
toksly <nwizutoks@gmail.com>
2021-04-01 05:45:43 -0500

    added some projects in my portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -6,34 +6,41 @@ function Portfolio() {
 	const [projects] = useState([
 		{
 			name: "password-generator",
-			description:
-				"Javascript app that generates random password",
+			description: "Javascript/CSS app that generates random password",
 			link: "https://toksly.github.io/Password-Generator/",
 			repo: "https://github.com/toksly/Password-Generator/",
 		},
+
 		{
-			name: "pastel-puzzels",
-			description: "MERN Stack",
-			link: "https://github.com",
-			repo: "https://github.com",
+			name: "spl-goalden",
+			description:
+				"App speech pathologists uses to track students goals and progress ",
+			link: "https://mighty-falls-97052.herokuapp.com/",
+			repo: "https://github.com/Craig5117/slp-goalden",
 		},
 		{
-			name: "run-buddy",
-			description: "HTML/CSS",
-			link: "https://github.com",
-			repo: "https://github.com",
+			name: "work-day-scheduler",
+			description: "Javascript/CSS",
+			link: "https://toksly.github.io/Work-day-Scheduler/",
+			repo: "https://github.com/toksly/Work-day-Scheduler",
 		},
 		{
-			name: "led-wall",
-			description: "Node/IoT",
-			link: "https://github.com",
-			repo: "https://github.com",
+			name: "code-quiz",
+			description: "Javascript/CSS app for generating timed quiz",
+			link: "https://toksly.github.io/Code-quiz/",
+			repo: "https://github.com/toksly/Code-quiz",
 		},
 		{
-			name: "calculator",
-			description: "React/JavaScript/CSS",
-			link: "https://github.com",
-			repo: "https://github.com",
+			name: "book-search-engine",
+			description: "MERN Stack app for searching books",
+			link: "https://toksly-book-search-engine.herokuapp.com/",
+			repo: "https://github.com/toksly/book-search-engine",
+		},
+		{
+			name: "weather-dashboard",
+			description: "JavaScript/CSS/API app for making weather forcast",
+			link: "https://toksly.github.io/Weather-Dashboard//github.com",
+			repo: "https://github.com/toksly/Weather-Dashboard",
 		},
 	]);
 
