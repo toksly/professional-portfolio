bb8296089629c11e0a6b2ad4d8c26a106ef24ea0
toksly <nwizutoks@gmail.com>
2021-04-14 10:06:43 -0500

    updated my about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -15,17 +15,18 @@ function About() {
 				</div>
 				<p>
 					My name is Tochukwu Sylvester Nwizu. I attended the Vanderbilt
-					University coding Boot Camp. As an up coming full stack web developer,
-					I am willing to work in an environment that will allow me to
-					contribute as well as help me to grow and become a successful full
-					stack web developer. I have a very strong work ethic, and I know the
+					University coding Boot Camp. As a full stack web developer,
+					I am looking to work in an environment that will allow me to contribute
+					as well as allow me to grow and develope as a full stack
+					web developer. I have a very strong work ethic, and I know the
 					importance of continuously striving for improvement. For this reason,
 					I am open and willing to trying new things outside of my comfort zone.
-					I intend using my new learned skill set in solving real world problems
-					through collaboration with my coleques. I believe with team work, no
-					problem will be too difficult to tackle. Moreover, My experience
-					working in healthcare setting has helped me to cultivate the habit of
-					working calmly even in a highly stressful environment.
+					I intend on using my new learned skill set in solving real world problems
+					through collaboration with my coleques. I believe with teamwork; no
+					problem will be too difficult to tackle.
+					Moreover, My experience working in healthcare setting has helped me to
+				    cultivate the habit of working calmly even in a highly stressful environment
+					
 				</p>
 			</div>
 		</section>
