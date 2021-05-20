c69cd0ee8435ac6bb4eb302d6395d236048e0b31
toksly <nwizutoks@gmail.com>
2021-05-19 20:02:51 -0500

    updated nav, contact and portfolio

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import coverImage from '../../assets/cover/image-cover.jpg';
+import React from "react";
+import coverImage from "../../assets/cover/image-cover.jpg";
 
 function Header(props) {
-
-  return (
-    <header className="flex-row space-between px-1">
-      <h1>Sylvester Nwizu</h1>
-      <img src={coverImage} alt="wavy background" width="100%"></img>
-      {props.children}
-    </header>
-  );
+	return (
+		<div className="d-flex flex-column flex-md-row px-md-4 bg-white border-bottom shadow-sm custom-height">
+			<h1 className="my-0 mr-md-auto font-weight-normal text-dark text-center gree-box">
+				Sylvester Nwizu
+			</h1>
+			{props.children}
+		</div>
+	);
 }
 
 export default Header;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,37 +1,31 @@
-import React, { useEffect } from 'react';
-import { capitalizeFirstLetter } from '../../utils/helpers';
+import React, { useEffect } from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
-  const {
-    pages = [],
-    setCurrentPage,
-    currentPage,
-  } = props;
+	const { pages = [], setCurrentPage, currentPage } = props;
 
-  useEffect(() => {
-    document.title = capitalizeFirstLetter(currentPage.name);
-  }, [currentPage]);
+	useEffect(() => {
+		document.title = capitalizeFirstLetter(currentPage.name);
+	}, [currentPage]);
 
-  return (    
-      <nav>
-        <ul className="flex-row">
-          {pages.map((Page) => (
-            <li
-              className={`mx-5 ${
-                currentPage.name === Page.name && 'navActive'
-                }`}
-              key={Page.name}
-            >
-              <span
-                onClick={() => setCurrentPage(Page)}
-              >
-                {capitalizeFirstLetter(Page.name)}
-              </span>
-            </li>
-          ))}
-        </ul>
-      </nav>
-  );
+	return (
+		<nav className="my-md-0 mr-md-3 navbar-stuff">
+			<ul>
+				{pages.map((Page) => (
+					<li
+						className={`p-2 text-dark ${
+							currentPage.name === Page.name && "navActive"
+						}`}
+						key={Page.name}
+					>
+						<span onClick={() => setCurrentPage(Page)}>
+							{capitalizeFirstLetter(Page.name)}
+						</span>
+					</li>
+				))}
+			</ul>
+		</nav>
+	);
 }
 
 export default Nav;
